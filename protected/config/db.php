b52af17cb9321592ba2f4c15a7a3ca64e80d69ea
Serov Alexander <serov.sh@gmail.com>
2011-09-16 19:55:18 +0400

    начальная правка конфигов, БД в отдельный фаил

---
+++ protected/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+        'connectionString' => 'mysql:host=localhost;dbname=muba2',
+        'emulatePrepare' => true,
+        'username' => 'user',
+        'password' => 'pass',
+        'charset' => 'utf8',
+);
--- protected/config/frontend.php
+++ protected/config/frontend.php
@@ -1,2 +1,27 @@
 <?php
+$config =  CMap::mergeArray(
+        require(dirname(__FILE__).'/core.php'),
+        array(
+        'components'=>array(
+               // 'user'=>array(
+                // enable cookie-based authentication
+               //         'allowAutoLogin'=>true,
+               // ),
+                // uncomment the following to enable URLs in path-format
+
+                'urlManager'=>array(
+                        'rules'=>array(
+                                '' => 'frontend/index',
+                        ),
+                ),
+
+
+                'errorHandler'=>array(
+                // use 'site/error' action to display errors
+                  'errorAction'=>'frontend/error',
+                ),
+        ),
+        )
+);
+return $config;
 ?>
