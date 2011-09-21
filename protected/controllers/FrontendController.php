fa3066d80b05a84807d7b5e377d3e0d20d710c3f
Serov Alexander <serov.sh@gmail.com>
2011-09-21 15:14:48 +0400

    Играл с layout

--- protected/controllers/FrontendController.php
+++ protected/controllers/FrontendController.php
@@ -27,6 +27,7 @@ class FrontendController extends SEController {
         // renders the view file 'protected/views/site/index.php'
         // using the default layout 'protected/views/layouts/main.php'
         
+        
         $this->render('index');
     }
 
