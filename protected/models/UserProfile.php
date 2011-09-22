7a3e2b1dd2d991e2702c616601a5a5b96262af10
Serov Alexander <serov.sh@gmail.com>
2011-09-22 10:30:29 +0400

    добавлены связи между моделями

--- protected/models/UserProfile.php
+++ protected/models/UserProfile.php
@@ -71,6 +71,7 @@ class UserProfile extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+                    'user'=>array(self::BELONGS_TO, 'User', 'id_user'),
 		);
 	}
 
